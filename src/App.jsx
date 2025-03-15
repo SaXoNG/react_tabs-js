a4b7ff81c95b2396f6aa0f298654143d6ffd14b6
Vasyl <priymak17ukrvaha@gmail.com>
2025-03-15 19:09:41 +0200

    render code

--- src/App.jsx
+++ src/App.jsx
@@ -11,45 +11,53 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => {
-  const [currentDivText, setCurrentTabText] = useState('Some text 1');
-  const [currentTItle, setCurrentTitle] = useState('Tab 1');
+export const App = ({ onTabSelected }) => {
+  const [currentTabId, setCurrentTabId] = useState(tabs[0].id);
+  const [currentTabText, setCurrentTabText] = useState(tabs[0].content);
+
+  const handleTabClick = tab => {
+    if (currentTabId === tab.id) return;
+
+    setCurrentTabId(tab.id);
+    setCurrentTabText(tab.content);
+
+    if (onTabSelected) {
+      onTabSelected(tab.id);
+    }
+  };
 
   return (
     <div className="section">
-      <h1 className="title">{`Selected tab is ${currentTItle}`}</h1>
+      <h1 className="title">{`Selected tab is ${tabs.find(tab => tab.id === currentTabId)?.title}`}</h1>
 
       <div data-cy="TabsComponent">
         <div className="tabs is-boxed">
           <ul>
-            {tabs.map(tab => {
-              return (
-                <li
-                  className={cn('', {
-                    'is-active': currentDivText === tab.content,
-                  })}
-                  data-cy="Tab"
-                  key={tab.id}
+            {tabs.map(tab => (
+              <li
+                className={cn('', {
+                  'is-active': currentTabId === tab.id,
+                })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  onClick={e => {
+                    e.preventDefault();
+                    handleTabClick(tab);
+                  }}
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
                 >
-                  <a
-                    onClick={e => {
-                      e.preventDefault();
-                      setCurrentTabText(tab.content);
-                      setCurrentTitle(tab.title);
-                    }}
-                    href={tab.id}
-                    data-cy="TabLink"
-                  >
-                    {tab.title}
-                  </a>
-                </li>
-              );
-            })}
+                  {tab.title}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
 
         <div className="block" data-cy="TabContent">
-          {currentDivText}
+          {currentTabText}
         </div>
       </div>
     </div>
